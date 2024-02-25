1223ff3f0985c49db3cc3cc27b313fda0c0c0bd1
sheikhartin <sheikhartin@gmail.com>
2024-02-25 18:11:14 +0330

    This is just a warm-up

---
+++ farr/__init__.py
@@ -0,0 +1,8 @@
+# Farr's goal is to give programmers the sense of liberation that comes
+# from the beauty of the code itself, even if it hurts productivity!
+# We understand that beauty is not objective...
+# https://github.com/sheikhartin/farr
+
+__author__ = 'Artin Mohammadi <sheikhartin@gmail.com>'
+__license__ = 'MIT'
+__version__ = '0.1.0'
