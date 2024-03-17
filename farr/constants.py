6f2c5641a489367ba860c23208bb2d8d32b0f054
sheikhartin <sheikhartin@gmail.com>
2024-03-17 13:00:23 +0330

    Constants to simplify maintenance

---
+++ farr/constants.py
@@ -0,0 +1,10 @@
+# Farr's goal is to give programmers the sense of liberation that comes
+# from the beauty of the code itself, even if it hurts productivity!
+# We understand that beauty is not objective...
+# https://github.com/sheikhartin/farr
+
+RESOURCES_ROOT_PATH = 'FARRPATH'  # Get from environment variables
+
+FILE_EXTENSION = 'farr'
+
+LIBRARY_INITIALIZER_FILE = f'funda.{FILE_EXTENSION}'
